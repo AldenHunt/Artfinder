87cb7f6b878a6d4800534bbe9b7671dbe3324930
AldenHunt <43453409+AldenHunt@users.noreply.github.com>
2019-03-13 12:45:45 -0400

    Full barebones server

--- artfinder.py
+++ artfinder.py
@@ -1,2 +1,17 @@
 from flask import Flask, url_for, render_template, request, make_response
 app = Flask(__name__)
+
+# This will be our splash page
+@app.route('/')
+def splash():
+    return render_template('hello.html')
+
+# This is our main map page
+@app.route('/map')
+def map():
+    return "This will be a map..."
+
+# However we're going to display the full data about an object
+@app.route('/objects/<int:obj_id>')
+def show_object_data(obj_id):
+    return 'ID: %s' % obj_id
