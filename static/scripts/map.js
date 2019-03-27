d881a40b77eb1fefe39aeae2e154ef6ac858dd68
AldenHunt <43453409+AldenHunt@users.noreply.github.com>
2019-03-27 18:01:28 -0400

    Minimal geolocation

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -4,9 +4,18 @@ corner2 = L.latLng(40.34, -74.664),
 bounds = L.latLngBounds(corner1, corner2);
 
 var mymap = L.map('mapid', {maxBounds: bounds,}).setView([40.3452, -74.6561], 17);
+mymap.locate({setView: true, maxZoom: 18});
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 20,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiYWNodW50IiwiYSI6ImNqdGF4Ym5odzBmbTE0M2w4ZGpyamhsbWEifQ.2wopZqSc9U4D3jIaWcQnIg',
 }).addTo(mymap);
+
+//Mark the current location (from Leaflet tutorial)
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    L.circle(e.latlng, radius).addTo(mymap);
+}
+
+mymap.on('locationfound', onLocationFound);
