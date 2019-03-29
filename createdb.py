bea63f79e25e2dfe98f9196658aa38fbb584d0b4
Rhea Braun <rhea.m.braun@gmail.com>
2019-03-29 00:16:25 -0400

    created database and data handling functions

---
+++ createdb.py
@@ -0,0 +1,107 @@
+# createdb.py
+# python suite to create database from Princeton Art Library Data
+# Rhea Braun
+
+import csv
+from urllib.request import urlopen
+import json
+import sqlite3
+from sqlite3 import Error
+ 
+# from sqlite3 tutorials on www.sqlitetutorial.net
+def create_connection(db_file):
+    # create a database connection to a SQLite database 
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return None
+ 
+def create_table(conn):
+    # create a table from the create_table_sql statement
+	create_table_sql = """ CREATE TABLE IF NOT EXISTS objects (
+									objectid integer PRIMARY KEY,
+									title text,
+									creators text,
+									dates text,
+									description text,
+									image text,
+									lat real,
+									long real
+								); """
+	try:
+		c = conn.cursor()
+		c.execute(create_table_sql)
+	except Error as e:
+		print(e)
+
+def populate_table(conn):
+	# load object ids and locations from csv
+	# from python csv documentation example code
+	insert = ''' INSERT INTO objects(objectid,title,lat,long)
+              VALUES(?,?,?,?) '''
+	with open('campus_art.csv', newline='') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			info = [int(row[0]), row[2], float(row[3]), float(row[4])]
+			print(info)
+			cur = conn.cursor()
+			cur.execute(insert, info)
+		conn.commit()
+		
+def update_table(conn):
+	lib = "https://data.artmuseum.princeton.edu/objects/"
+	update= ''' UPDATE objects
+              SET creators = ? ,
+                  dates = ? ,
+                  description = ?,
+                  image = ? 
+              WHERE objectid = ?'''
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM objects")
+	rows = cur.fetchall()
+	for row in rows:
+		id = row[0]
+		url = lib + str(id)
+		info = json.loads(urlopen(url).read())
+		try:
+			makers = info["makers"][0]["displayname"]
+			date = info["displaydate"]
+			description = info["texts"][0]["textentryhtml"]
+			image = info["media"][0]["uri"]
+		except:
+			image = ""
+			print("missing data: " + str(id))
+		info = [makers, date, description, image, id]
+		#print(info)
+		cur.execute(update, info)
+	conn.commit()
+		
+def display_table(conn):
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM objects")
+	rows = cur.fetchall()
+	for row in rows:
+		print(row)
+
+def main():
+	database = "artobjects.db"
+
+	# create a database connection
+	conn = create_connection(database)
+	if conn is not None:
+		# create objects table
+		create_table(conn)
+		# add items
+		populate_table(conn)
+		update_table(conn)
+		
+		# display the created table
+		display_table(conn)
+		conn.close()
+	else:
+		print("Error! cannot create the database connection.")
+	
+# running this code with a preexisting database may cause unintended consequences	
+#main()
---
+++ usedb.py
@@ -0,0 +1,24 @@
+# usedb.py
+# a collection of sql methods to return data from the database
+# untested
+# Rhea Braun
+import sqlite3
+
+def display_objects_table(db_file):
+	conn = sqlite3.connect(db_file)
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM objects")
+	rows = cur.fetchall()
+	for row in rows:
+		print(row)
+	conn.close()
+	
+def display_object_data(db_file, objectid):
+	conn = sqlite3.connect(db_file)
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM objects WHERE objectid=?", (objectid,))
+	rows = cur.fetchall()
+	#for row in rows:
+	#	print(row)
+	conn.close()
+	return rows
