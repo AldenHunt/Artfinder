303281b9cb6b9fed5d6d60d2742438d82b1d75c9
AldenHunt <43453409+AldenHunt@users.noreply.github.com>
2019-04-10 18:13:43 -0400

    Reverted bad changes and added links
    We have established our core functionality!

--- artfinder.py
+++ artfinder.py
@@ -14,12 +14,12 @@ def splash():
 @app.route('/map', methods = ['GET'])
 def map():
     objects = usedb.json_objects_table('artobjects.db')
-    print(objects)
     return render_template('map.html', objects=objects)
 
 # However we're going to display the full data about an object
 @app.route('/objects/<int:obj_id>')
 def show_object_data(obj_id):
-    return 'ID: %s' % obj_id
+    objdata = usedb.display_object_data('artobjects.db', obj_id)
+    return render_template('objects.html', objdata=objdata)
 
 
