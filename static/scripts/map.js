793faa5b687f7b6f595ffb16e645f88ddc2130f5
AldenHunt <43453409+AldenHunt@users.noreply.github.com>
2019-04-09 20:54:15 -0400

    Making zoom work right in map.js

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -3,8 +3,7 @@ var corner1 = L.latLng(40.3520, -74.647),
 corner2 = L.latLng(40.34, -74.664),
 bounds = L.latLngBounds(corner1, corner2);
 
-var mymap = L.map('mapid', {maxBounds: bounds, minZoom: 15, maxZoom: 19}).setView([40.3474, -74.6581], 18);
-mymap.locate({setView: true, enableHighAccuracy: true});
+var mymap = L.map('mapid', {maxBounds: bounds, minZoom: 15, maxZoom: 19}).setView([40.3474, -74.6581], 17);
 
 /* We're currently getting our tiles (the actual map rendering) from
 Mapbox. If this app ever gets big, we really should change providers
@@ -16,24 +15,32 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiYWNodW50IiwiYSI6ImNqdGF4Ym5odzBmbTE0M2w4ZGpyamhsbWEifQ.2wopZqSc9U4D3jIaWcQnIg',
 }).addTo(mymap);
 
+
+
 //Mark the current location (from Leaflet tutorial)
 function onLocationFound(e) {
-    mymap.stopLocate();
-    var radius = e.accuracy / 2;
+    console.log("Location worked");
+    var radius = e.accuracy/2;
+    console.log("Radius: " + radius);
     // Only print circle if pretty sure in the location panned to
     if (radius < 50) {
+        
         L.circle(e.latlng, radius).addTo(mymap);
     }
     else {
-        mymap.setView([40.3474, -74.6581], 18);
+        console.log("Setting view back");
+        mymap.flyTo([40.3474, -74.6581], 18);
+        console.log("Did the fly");
     }
 }
 
-mymap.on('locationfound', onLocationFound);
-
 function onLocationError(e) {
-    mymap.setView([40.3474, -74.6581], 18);
+    console.log("Location failed");
+    mymap.flyTo([40.3474, -74.6581], 18);
 }
 
-map.on('locationerror', onLocationError);
+mymap.on('locationfound', onLocationFound);
+mymap.on('locationerror', onLocationError);
+
+mymap.locate({setView: true, enableHighAccuracy: true}); 
 
