27962f7ff07ca8e2eec1d7b1fcd0ba277d81e06f
AldenHunt <43453409+AldenHunt@users.noreply.github.com>
2019-04-11 22:00:07 -0400

    Fixing bad merges

--- artfinder.py
+++ artfinder.py
@@ -2,7 +2,6 @@ from flask import Flask, url_for, render_template, request, make_response, Marku
 from flask_sslify import SSLify
 import usedb
 
-
 app = Flask(__name__)
 sslify = SSLify(app)
 
@@ -12,15 +11,23 @@ def splash():
     return render_template('hello.html')
 
 # This is our main map page
-@app.route('/map')
+@app.route('/map', methods = ['GET'])
 def map():
     objects = usedb.json_objects_table('artobjects.db')
+<<<<<<< HEAD
+=======
+    print(objects)
+>>>>>>> parent of 2009e3c... Revert "Merge branch 'sadiebranch'"
     return render_template('map.html', objects=objects)
 
 # However we're going to display the full data about an object
 @app.route('/objects/<int:obj_id>')
 def show_object_data(obj_id):
+<<<<<<< HEAD
     objdata = usedb.display_object_data('artobjects.db', obj_id)
     return render_template('objects.html', objdata=objdata)
+=======
+    return 'ID: %s' % obj_id
+>>>>>>> parent of 2009e3c... Revert "Merge branch 'sadiebranch'"
 
 
--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,5 +1,5 @@
 // Initial Leaflet javascript - get map and center on Princeton
-var corner1 = L.latLng(40.3520, -74.647),
+var corner1 = L.latLng(40.3520, -74.6475),
 corner2 = L.latLng(40.34, -74.664),
 bounds = L.latLngBounds(corner1, corner2);
 
@@ -38,6 +38,7 @@ function onLocationError(e) {
 mymap.on('locationfound', onLocationFound);
 mymap.on('locationerror', onLocationError);
 
+<<<<<<< HEAD
 /* Automatically locates the user and sets the view to their location) */
 mymap.locate({setView: true, enableHighAccuracy: true});
 
@@ -55,4 +56,26 @@ function addMarkers(){
     }
 }
 
+=======
+mymap.locate({setView: true, enableHighAccuracy: true}); 
+    var radius = e.accuracy / 2;
+    L.circle(e.latlng, radius).addTo(mymap);
+}
+
+function addMarkers(){
+    window.alert(objects);
+    var objdata = JSON.parse(objects);
+
+    //JSON.parse(document.getElementById("#mydiv").data("objects"));
+
+
+    for (item in objdata) {
+        var marker = L.marker([objdata[item]["lat"], objdata[item]["long"]]).addTo(mymap);
+        var data = ("<b>" + objdata[item]["title"] + "</b><br>" + objdata[item]["creators"])
+        marker.bindPopup(data).openPopup()
+    }
+}
+
+mymap.on('locationfound', onLocationFound);
+>>>>>>> parent of 2009e3c... Revert "Merge branch 'sadiebranch'"
 addMarkers();
